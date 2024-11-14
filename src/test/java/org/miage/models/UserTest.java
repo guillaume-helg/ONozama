f833cfe6eaee43144ed046c7ec0530fe762db738
guillaume-helg <guillaume.helg@protonmail.com>
2024-11-14 12:53:44 +0100

    test: test user finished

--- src/test/java/org/miage/models/UserTest.java
+++ src/test/java/org/miage/models/UserTest.java
@@ -1,17 +1,32 @@
 package org.miage.models;
 
+import org.junit.Before;
+import org.miage.models.accounts.User;
+
+import static org.junit.Assert.assertEquals;
+
 public class UserTest {
+
+    private User user;
+
+    @Before
+    public void setUp() throws Exception {
+        this.user = new User("Roche", "Bleu") {
+            @Override
+            public void displayMenu() {
+                System.out.println("salut");
+            }
+        };
+    }
 
     @org.junit.Test
     public void getNomUtilisateur() {
-
+        assertEquals("Roche", this.user.getIdUser());
     }
 
     @org.junit.Test
     public void getMotDePasse() {
+        assertEquals("Bleu", this.user.getPassword());
     }
 
-    @org.junit.Test
-    public void displayMenu() {
-    }
 }
